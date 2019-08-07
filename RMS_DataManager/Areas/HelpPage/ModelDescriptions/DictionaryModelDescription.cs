aa59a7ae2bc42fd3644bcf0f0c1705219bdb237c
Navaneeth Reddy Matta <mattan@netscout.com>
2019-08-06 20:49:35 -0400

    Swagger Setup to RMS Solution

---
+++ RMS_DataManager/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RMS_DataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ RMS_DataManager/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(RMS_DataManager.Startup))]
+
+namespace RMS_DataManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
